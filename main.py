ce085837529c7477a36fa9bef778eec2f97d860e
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 08:36:23 -0700

    Assignment 7-4

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-
-
+# List of numbers
 numbers = [5, 20, 30, 30, 50]
+
+# Get value to delete from user
 delval = int(input('Enter the deletion value: '))
 
-# ******************************
-# Make your Code
-# ******************************
+# Remove value from list
+try:
+    for i in range(2):
+        delIndex = numbers.index(delval)
+        del numbers[delIndex]
+except ValueError:
+    print("Value does not exist in list.") 
 
+# Print updated list
 print (numbers)
